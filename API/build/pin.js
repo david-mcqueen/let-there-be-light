dc53a3afebb6180cb2a0804d5241a0688df3bb90
David McQueen <david@dmcqueen.co.uk>
2020-05-08 14:55:33 +0100

    Remove build files

--- API/build/pin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Pin;
-(function (Pin) {
-    Pin[Pin["WARM_WHITE"] = 17] = "WARM_WHITE";
-    Pin[Pin["COOL_WHITE"] = 22] = "COOL_WHITE";
-})(Pin || (Pin = {}));
-exports.default = Pin;
--- API/build/pins.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Pins;
-(function (Pins) {
-    Pins[Pins["WARM_WHITE"] = 17] = "WARM_WHITE";
-    Pins[Pins["COOL_WHITE"] = 22] = "COOL_WHITE";
-})(Pins || (Pins = {}));
-exports.default = Pins;
