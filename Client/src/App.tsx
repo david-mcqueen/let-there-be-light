4d96fdca013fcde529f0994736423e77f5ee9ac0
David McQueen <david@dmcqueen.co.uk>
2020-05-10 11:55:22 +0100

    Init client

---
+++ Client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Schedule from './Components/Schedule';
+
+function App() {
+  return (
+    <div className="App">
+      <Schedule/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/Components/Schedule.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function Schedule() {
+
+
+    return (
+        <div className="slider">
+            Slider
+        </div>
+    )
+
+}
+
+
+export default Schedule;
