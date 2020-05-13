5a5ba5f868bff561e7a615c1901b34280e9f5aa7
David McQueen <david@dmcqueen.co.uk>
2020-05-13 22:17:06 +0100

    Modify the sleep script
    - Start from half way
    - DOn't include the CW channel

--- Scripts/light-off.py
+++ Scripts/light-off.py
@@ -11,17 +11,13 @@ epochDelay = sec / maxvalue
 
 print(epochDelay)
 
-for i in range(maxvalue - 1, 0, -1):
-	subprocess.check_call(['pigs', 'p 17', str(i)])
-	if i > midpoint:
-		cwValue = (i - midpoint) * 2
-		subprocess.check_call(['pigs', 'p 22', str(cwValue)])
-		print(str(cwValue))
-	else:
-		subprocess.check_call(['pigs', 'p 22', str(0)])
+subprocess.check_call(['pigs', 'p 22', str(0)])
 
+for i in range(maxvalue / 2, 0, -1):
+    	subprocess.check_call(['pigs', 'p 17', str(i)])
+	
 	print(str(i))
-	time.sleep(epochDelay)
+	time.sleep(epochDelay * 2)
 
 subprocess.check_call(['pigs', 'p 17', str(0)])
 elapsed_time = time.time() - start_time
